0ca7408216ff6614041fc05860efcc5c9e7187d1
ashhanai <ashhab.naim@gmail.com>
2024-06-13 09:48:03 +0200

    feat: implement aave pool adapter

---
+++ src/pool-adapter/AaveAdapter.sol
@@ -0,0 +1,75 @@
+// SPDX-License-Identifier: GPL-3.0-only
+pragma solidity 0.8.16;
+
+import { MultiToken } from "MultiToken/MultiToken.sol";
+
+import { PWNHub } from "pwn/hub/PWNHub.sol";
+import { PWNHubTags } from "pwn/hub/PWNHubTags.sol";
+import { IPoolAdapter } from "pwn/interfaces/IPoolAdapter.sol";
+import { AddressMissingHubTag } from "pwn/PWNErrors.sol";
+
+
+interface IAavePoolLike {
+    struct ReserveData {
+        uint256 reserveConfigurationMap;
+        uint128 liquidityIndex;
+        uint128 currentLiquidityRate;
+        uint128 variableBorrowIndex;
+        uint128 currentVariableBorrowRate;
+        uint128 currentStableBorrowRate;
+        uint40 lastUpdateTimestamp;
+        uint16 id;
+        address aTokenAddress;
+        address stableDebtTokenAddress;
+        address variableDebtTokenAddress;
+        address interestRateStrategyAddress;
+        uint128 accruedToTreasury;
+        uint128 unbacked;
+        uint128 isolationModeTotalDebt;
+    }
+
+    function getReserveData(address asset) external view returns (ReserveData memory);
+
+    function withdraw(address asset, uint256 amount, address to) external returns (uint256);
+    function supply(address asset, uint256 amount, address onBehalfOf, uint16 referralCode) external;
+}
+
+contract AaveAdapter is IPoolAdapter {
+    using MultiToken for MultiToken.Asset;
+    using MultiToken for address;
+
+    PWNHub public hub;
+
+    constructor(address _hub) {
+        hub = PWNHub(_hub);
+    }
+
+    /**
+     * @inheritdoc IPoolAdapter
+     */
+    function withdraw(address pool, address owner, address asset, uint256 amount) external {
+        // Check caller is active loan contract
+        if (!hub.hasTag(msg.sender, PWNHubTags.ACTIVE_LOAN)) {
+            revert AddressMissingHubTag({ addr: msg.sender, tag: PWNHubTags.ACTIVE_LOAN });
+        }
+
+        // Transfer aTokens to this contract
+        IAavePoolLike(pool)
+            .getReserveData(asset).aTokenAddress
+            .ERC20(amount) // Note: Assuming aToken is minted in 1:1 ratio to underlying asset
+            .transferAssetFrom(owner, address(this));
+
+        // Withdraw from the pool to the owner
+        IAavePoolLike(pool).withdraw(asset, amount, owner);
+    }
+
+    /**
+     * @inheritdoc IPoolAdapter
+     */
+    function supply(address pool, address owner, address asset, uint256 amount) external {
+        // Supply to the pool on behalf of the owner
+        asset.ERC20(amount).approveAsset(pool);
+        IAavePoolLike(pool).supply(asset, amount, owner, 0);
+    }
+
+}
