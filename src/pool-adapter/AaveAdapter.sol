122ca7fbc61b5032b0cc1daf0a262f446750f07b
ashhanai <ashhab.naim@gmail.com>
2025-03-05 10:19:08 +0100

    test: separate aave pool interface into standalone file

--- src/pool-adapter/AaveAdapter.sol
+++ src/pool-adapter/AaveAdapter.sol
@@ -8,44 +8,9 @@ import { PWNHubTags } from "pwn/hub/PWNHubTags.sol";
 import { IPoolAdapter } from "pwn/interfaces/IPoolAdapter.sol";
 import { AddressMissingHubTag } from "pwn/PWNErrors.sol";
 
+import { IAavePoolLike } from "src/interfaces/IAavePoolLike.sol";
 import { checkInputs } from "src/pool-adapter/utils/checkInputs.sol";
 
-
-interface IAavePoolLike {
-    struct ReserveData {
-        uint256 reserveConfigurationMap;
-        uint128 liquidityIndex;
-        uint128 currentLiquidityRate;
-        uint128 variableBorrowIndex;
-        uint128 currentVariableBorrowRate;
-        uint128 currentStableBorrowRate;
-        uint40 lastUpdateTimestamp;
-        uint16 id;
-        address aTokenAddress;
-        address stableDebtTokenAddress;
-        address variableDebtTokenAddress;
-        address interestRateStrategyAddress;
-        uint128 accruedToTreasury;
-        uint128 unbacked;
-        uint128 isolationModeTotalDebt;
-    }
-
-    function getReserveData(address asset) external view returns (ReserveData memory);
-
-    function withdraw(address asset, uint256 amount, address to) external returns (uint256);
-    function supply(address asset, uint256 amount, address onBehalfOf, uint16 referralCode) external;
-
-    function borrow(address asset, uint256 amount, uint256 interestRateMode, uint16 referralCode, address onBehalfOf) external;
-
-    function getUserAccountData(address user) external view returns (
-        uint256 totalCollateralBase,
-        uint256 totalDebtBase,
-        uint256 availableBorrowsBase,
-        uint256 currentLiquidationThreshold,
-        uint256 ltv,
-        uint256 healthFactor
-    );
-}
 
 contract AaveAdapter is IPoolAdapter {
     using MultiToken for MultiToken.Asset;
